94649afe05cf48c73af6ffc456a4b6755f6aa985
Fabián Rojas <forf1997@hotmail.com>
2020-12-15 18:36:50 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//setup vars
+const firstGraphicsCard={
+  model: 'Nvidia 3080',
+  manufacturer: "Nvidia",
+  img: 'https://i.blogs.es/0f4e3a/rtx3080-1/200_200.jpg'
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const secondGraphicsCard = {
+  model: "Nvidia 3070",
+  manufacturer: 'Nvidia',
+  img: 'https://www.scan.co.uk/images/products/3217723-a.jpg',
+}
+
+function GraphicsCardList(){
+  return (
+    <section className="graphicsCardList">
+        <GraphicsCard model={firstGraphicsCard.model} manufacturer={firstGraphicsCard.manufacturer} img={firstGraphicsCard.img} />
+        <GraphicsCard model={secondGraphicsCard.model} manufacturer={secondGraphicsCard.manufacturer} img={secondGraphicsCard.img} />
+    </section>
+  );
+}
+
+const GraphicsCard = ({img, model, manufacturer}) => {
+  return (
+  <article className="graphicsCard">
+    <img src={img} alt="THE 3080"></img>
+    <h2>{model}</h2>
+    <h4>{manufacturer.toUpperCase()}</h4>
+  </article>
+  )};
+
+ReactDom.render(<GraphicsCardList />, document.getElementById("root"));
