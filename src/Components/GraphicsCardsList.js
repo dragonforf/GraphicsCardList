3b8ea7629f858b04e93a3d7eac8e3d14fb17f7d2
Fabián Rojas <forf1997@hotmail.com>
2021-09-16 22:19:19 -0500

    A comment was added to test git from terminal

--- src/Components/GraphicsCardsList.js
+++ src/Components/GraphicsCardsList.js
@@ -1,10 +1,10 @@
 import GraphicsCard from './GraphicsCard';
-
+//This is a terminal test
 const GraphicsCardsList = (props) => {
   let noMatchText = (props.searchedValue) ? <span style={{color: 'darkred'}}>No match found for {props.searchedValue}</span> :"";
   return (
       <section className="graphicsCardList">
-        {noMatchText}
+        {noMatchText} 
         {props.theList.map( gc => {
           return <GraphicsCard key={gc.id} {...gc} />;
         })}
